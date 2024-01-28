226c888cbef0da38d7f75e5ad1960158ee861ff3
RushiVivek <rushivivek123@gmail.com>
2024-01-29 01:01:56 +0530

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // The entry point file described above
-  entry: './public/script.js',
+  entry: './public/index.js',
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'public'),
